e15fe6ffa9395ec452564c3cbf060b371bfe5bdc
lgustavoss <seu-email@exemplo.com>
2025-09-27 17:14:39 -0300

    fix(ws): importar consumidores apos django.setup() no ASGI

--- config/asgi.py
+++ config/asgi.py
@@ -3,11 +3,12 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
 
-from core.ws import EchoConsumer, jwt_auth_middleware
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", os.getenv("DJANGO_SETTINGS_MODULE", "config.settings.development"))
 
 django_asgi_app = get_asgi_application()
+
+# Importar consumidores/middleware APÓS inicializar o Django
+from core.ws import EchoConsumer, jwt_auth_middleware  # noqa: E402
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
